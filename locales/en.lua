3f953842ee5e2c0825a1c6269c22b7c9b1508286
SifoGaming <124735333+sifosifosifo@users.noreply.github.com>
2026-05-22 13:48:27 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,27 @@
+Locales = Locales or {}
+
+Locales['en'] = {
+
+    enter_zone = 'You entered a safe zone',
+    exit_zone = 'You left the safe zone',
+
+    no_permission = 'You do not have permission',
+
+    create_zone = 'Create Green Zone',
+    manage_zones = 'Manage Zones',
+
+    waypoint_help = 'Set waypoint then press E',
+
+    zone_created = 'Zone created successfully',
+
+    need_waypoint = 'You must place a waypoint',
+
+    edit_radius = 'Edit Radius',
+
+    delete_zone = 'Delete Zone',
+
+    enable_zone = 'Enable Zone',
+
+    disable_zone = 'Disable Zone'
+
+}
---
+++ locales/es.lua
@@ -0,0 +1,27 @@
+Locales = Locales or {}
+
+Locales['es'] = {
+
+    enter_zone = 'Entraste en una zona segura',
+    exit_zone = 'Saliste de la zona segura',
+
+    no_permission = 'No tienes permisos',
+
+    create_zone = 'Crear Zona Verde',
+    manage_zones = 'Administrar Zonas',
+
+    waypoint_help = 'Coloca un waypoint y presiona E',
+
+    zone_created = 'Zona creada correctamente',
+
+    need_waypoint = 'Debes colocar un waypoint',
+
+    edit_radius = 'Editar radio',
+
+    delete_zone = 'Eliminar zona',
+
+    enable_zone = 'Activar zona',
+
+    disable_zone = 'Desactivar zona'
+
+}
